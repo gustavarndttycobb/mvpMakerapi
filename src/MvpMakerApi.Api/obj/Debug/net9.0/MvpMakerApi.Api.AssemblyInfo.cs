ed86b5825dba4a8dec8b4472ed80ec37727d174a
Gustavo Arndt <gustavo.arndt@radixeng.com>
2025-12-08 09:18:10 -0300

    feat:env example

--- src/MvpMakerApi.Api/obj/Debug/net9.0/MvpMakerApi.Api.AssemblyInfo.cs
+++ src/MvpMakerApi.Api/obj/Debug/net9.0/MvpMakerApi.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvpMakerApi.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a18d713b088f53ede5d6946438160113a4802f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+957e42312279eee7e8e792748a837013f9363cce")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvpMakerApi.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvpMakerApi.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
